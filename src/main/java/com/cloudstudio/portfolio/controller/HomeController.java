39162c8d96916d33528145d4b99585143278f912
tumiso_cloud <tumisomphale14@gmail.com>
2026-09-14 01:00:07 +0200

    feat(controller): inject RoleService into HomeController
    - Wire RoleService dependency into HomeController constructor
    - Pass roles list to home template model for dynamic hero typewriter

--- src/main/java/com/cloudstudio/portfolio/controller/HomeController.java
+++ src/main/java/com/cloudstudio/portfolio/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.cloudstudio.portfolio.controller;
 
+import com.cloudstudio.portfolio.service.AboutService;
+import com.cloudstudio.portfolio.service.RoleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    // display home page
+
+    private final RoleService roleService;
+    private final AboutService aboutService;
+
+    public HomeController(RoleService roleService, AboutService aboutService) {
+        this.roleService = roleService;
+        this.aboutService = aboutService;
+    }
+
     @GetMapping("/")
-    public String viewHomePage(){
+    public String viewHomePage(Model model) {
+        model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("about", aboutService.getAboutContent());
         return "home";
     }
 }
